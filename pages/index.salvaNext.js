4d0359303641ad7cc547a6446275441a7f457a8a
Alberto Roman <romanvieito@gmail.com>
2018-07-21 11:15:33 -0400

    init next.js and hola

---
+++ pages/index.salvaNext.js
@@ -0,0 +1,21 @@
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Ava"/>
+      <PostLink title="Roman"/>
+      <PostLink title="Cute"/>
+    </ul>
+  </Layout>
+)
