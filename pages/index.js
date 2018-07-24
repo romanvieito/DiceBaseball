bf976a52beb1e3e552f51203e0aebcc0a94bf83d
Alberto Roman <romanvieito@gmail.com>
2018-07-23 20:42:22 -0400

    adding helpers and drawing bases

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
-import HistoryRender from "../components/HistoryRender"
-import Dices from "../components/Dices"
-import Bases from "../components/Bases"
+import HistoryRender from "../components/HistoryRender";
+import Dices from "../components/Dices";
+import Bases from "../components/Bases";
+import {drawBases} from "../helpers/batting";
 
 class Index extends React.Component {
 
   state = {
     historyDices: {},
+    bases: [ false, false, false],
     dice1: 1,
     dice2: 1,
-  };
-
-  static propTypes = {
-    historyDices: PropTypes.object
   };
 
   rollDice = event => {
     event.preventDefault();
     const randomNum1 = Math.ceil(Math.random() * 6);
     const randomNum2 = Math.ceil(Math.random() * 6);
-    this.addNewDiceNumber(randomNum1, randomNum2);
-    
+    this.addNewDiceNumberToHistory(randomNum1, randomNum2);
     
     let dice1 = { ...this.state.dice1 };
     let dice2 = { ...this.state.dice2 };
@@ -30,9 +27,12 @@ class Index extends React.Component {
     this.setState({ dice1 });
     this.setState({ dice2 });
 
+    let dicenumber = dice1;
+    if (dicenumber < dice2) dicenumber = dice2;
+    this.setState({ bases: drawBases(dicenumber, this.state.bases) });
   }
 
-  addNewDiceNumber = (numberDice1, numberDice2) => {
+  addNewDiceNumberToHistory = (numberDice1, numberDice2) => {
     const historyDices = { ...this.state.historyDices };
     historyDices[`dices${Date.now()}`] = [numberDice1, numberDice2];
     this.setState({ historyDices });
@@ -43,7 +43,7 @@ class Index extends React.Component {
       <React.Fragment>
         <button onClick={this.rollDice}>Roll dices→</button>
         <Dices valueDice1={this.state.dice1} valueDice2={this.state.dice2}/>
-        <Bases bases={[true, false, true]}/>
+        <Bases bases={this.state.bases}/>
         <ul>
           {Object.keys(this.state.historyDices).map(key => (
             <HistoryRender
