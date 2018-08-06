42828a070273116d382905f55272b0ba2770bc42
Alberto Roman <romanvieito@gmail.com>
2018-08-06 16:09:08 -0400

    adding lang to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,9 +9,9 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en-US">
         <Head>
-          <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
           <style>{`body { margin: 0 } /* custom! */`}</style>
         </Head>
         <body className="custom_class">
